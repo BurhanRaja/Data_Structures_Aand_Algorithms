8dda97971441c26b3dda15163b77262daef052bd
BurhanuddinRaja <mails4burhan@gmail.com>
2022-04-26 12:09:50 +0530

    Question added

---
+++ Solved Questions/Arrays/src/com/Burhan/Create_Target_Array.java
@@ -0,0 +1,30 @@
+package com.Burhan;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Create_Target_Array {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 3, 4};
+        int[] index = {0, 1, 2, 2, 1};
+        int[] ans = targetArray(nums, index);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    // nums = [0,1,2,3,4], index = [0,1,2,2,1]
+    static int[] targetArray(int[] nums, int[] index) {
+        int n = nums.length;
+        int[] target = new int[n];
+        ArrayList<Integer> list = new ArrayList<>();
+
+
+        for (int i =0; i<n; i++) {
+            list.add(index[i], nums[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            target[i] = list.get(i);
+        }
+        return target;
+    }
+}
