4adafcf809d9f50cd1b16b555a042712ad2f70f8
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-06 17:34:35 +0530

    PrettyPrint added

---
+++ String/src/com/Burhan/PrettyPrint.java
@@ -0,0 +1,23 @@
+package com.Burhan;
+
+public class PrettyPrint {
+    public static void main(String[] args) {
+        float a = 435.5965f;
+        System.out.printf("Pretty Printing the float: %.2f", a);
+
+        // There are different types of format placeholders.
+        // Below are some of them
+        
+        // %c - Character
+        // %d - Decimal number (base 10)
+        // %e - Exponential floating-point number
+        // %f - Floating-point number
+        // %i - Integer (base 10)
+        // %o - Octal number (base 8)
+        // %s - String
+        // %u - Unsigned decimal (integer) number
+        // %x - Hexadecimal number (base 16)
+        // %t - Date/time
+        // %n - Newline.
+    }
+}
