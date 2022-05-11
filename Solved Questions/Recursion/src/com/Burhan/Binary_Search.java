f86953950608d59572a6a863284cccde8b66ec3a
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-11 12:53:20 +0530

    Question added and one updated

--- Solved Questions/Recursion/src/com/Burhan/Binary_Search.java
+++ Solved Questions/Recursion/src/com/Burhan/Binary_Search.java
@@ -3,7 +3,7 @@ package com.Burhan;
 public class Binary_Search {
     public static void main(String[] args) {
         int[] arr = {1, 4, 7, 10, 13, 16, 20, 25, 39, 56};
-        int target = 16;
+        int target = 39;
         int start = 0;
         int end = arr.length-1;
         int ans = binarySearch(arr, target, start, end);
@@ -11,6 +11,9 @@ public class Binary_Search {
     }
 
     static int binarySearch(int[] arr, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
         int mid = start + (end-start)/2;
         if (target > arr[mid]) {
             start = mid+1;
