6bf14d5392ba06b3b785f826ba0a3b9f19f338b0
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-20 10:25:16 +0530

    OR Bitwise op added

---
+++ Maths for DSA/src/com/Burhan/OR_Operator.java
@@ -0,0 +1,18 @@
+package com.Burhan;
+
+public class OR_Operator {
+    public static void main(String[] args) {
+        // int num1 = 5;
+        // int num2 = 7;
+        
+        // the answer here is 111 i.e. 7.
+        // In AND operator the answer would have been 101 i.e. 5.
+
+        int num1 = 10;
+        int num2 = 29;
+        // The answer here is 11111 i.e. 31.
+        // In AND operator the answer would have been 1000 i.e. 8.
+
+        System.out.println((num1|num2));
+    }
+}
