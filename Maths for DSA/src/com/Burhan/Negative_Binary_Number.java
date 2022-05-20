a615e78b82000c819c37065f05bba81d97ae7b96
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-20 17:33:59 +0530

    Question added

---
+++ Maths for DSA/src/com/Burhan/Negative_Binary_Number.java
@@ -0,0 +1,13 @@
+package com.Burhan;
+
+public class Negative_Binary_Number {
+    public static void main(String[] args) {
+        int a = 10;
+        
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(a^1));
+
+
+        // Here, first the number given is complemented and then 
+    }
+}
