ea3fc5a08fdcf779166a400be9234e231cb739e0
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-20 11:09:43 +0530

    Right Shift Bitwise op added

---
+++ Maths for DSA/src/com/Burhan/Right_Shift_Operator.java
@@ -0,0 +1,11 @@
+package com.Burhan;
+
+public class Right_Shift_Operator {
+    public static void main(String[] args) {
+        int num = 5; // 101
+        // The answer will be 011 i.e. 2
+        // The one will shift towards right as per the given i i.e. >>i, i++.
+
+        System.out.println((num>>1));
+    }
+}
