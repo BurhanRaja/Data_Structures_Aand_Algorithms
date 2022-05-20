6413728e3903d4e09fa08c56385f9bf933bf1e91
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-20 10:37:46 +0530

    XOR Bitwise op added

---
+++ Maths for DSA/src/com/Burhan/XOR_Operator.java
@@ -0,0 +1,15 @@
+package com.Burhan;
+
+public class XOR_Operator {
+    public static void main(String[] args) {
+        // int num1 = 6; // 110, 7 is 111.
+        // Complement of 6 is 7 and 7 is 6.
+
+        int num1 = 2;
+        
+        System.out.println((num1^1)); // Complement
+        System.out.println((num1^0)); // number itself
+        System.out.println((num1^num1)); // Zero
+
+    }
+}
