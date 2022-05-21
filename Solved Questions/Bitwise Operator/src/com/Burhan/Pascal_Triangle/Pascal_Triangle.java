dd1dcfa172c5063242b6676ceec74ccd95a3c8e9
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-21 19:47:43 +0530

    Question added

---
+++ Solved Questions/Bitwise Operator/src/com/Burhan/Pascal_Triangle/Pascal_Triangle.java
@@ -0,0 +1,9 @@
+package com.Burhan.Pascal_Triangle;
+
+public class Pascal_Triangle {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(1<<(n-1));
+    }
+}
