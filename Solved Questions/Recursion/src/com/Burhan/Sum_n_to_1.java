1e57dfd28be9f195e5437a6969c6a38a53354e60
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-27 11:45:06 +0530

    Question added

---
+++ Solved Questions/Recursion/src/com/Burhan/Sum_n_to_1.java
@@ -0,0 +1,17 @@
+package com.Burhan;
+
+public class Sum_n_to_1 {
+    public static void main(String[] args) {
+        int n = 6;
+        int ans = sumNum(n);
+        System.out.println(ans);
+    }
+
+    static int sumNum(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        return n+sumNum(n-1);
+    }
+}
