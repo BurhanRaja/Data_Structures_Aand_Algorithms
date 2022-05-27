230a8d0516a95b9e47e8eaaf5e7654e07facd715
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-27 11:23:40 +0530

    Question added

---
+++ Solved Questions/Recursion/src/com/Burhan/Print_n_to_1.java
@@ -0,0 +1,16 @@
+package com.Burhan;
+
+public class Print_n_to_1 {
+    public static void main(String[] args) {
+        int n = 8;
+        printNum(n);
+    }
+
+    static void printNum(int n) {
+        if (n > 0) {
+            System.out.print(n + " ");
+            n--;
+            printNum(n);
+        }
+    }
+}
