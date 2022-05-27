7a1794798afa24a7be98a3ce83624f7c90b2d366
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-27 11:57:25 +0530

    Question added

---
+++ Solved Questions/Recursion/src/com/Burhan/Sum_Digits.java
@@ -0,0 +1,17 @@
+package com.Burhan;
+
+public class Sum_Digits {
+    public static void main(String[] args) {
+        int n = 24678;
+        int ans = sumDigits(n);
+        System.out.println(ans);
+    }
+
+    static int sumDigits(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        return (n%10) + sumDigits(n /= 10);
+
+    }
+}
