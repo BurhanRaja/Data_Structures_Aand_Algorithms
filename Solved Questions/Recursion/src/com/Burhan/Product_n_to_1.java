2869a6811f80f8cf9f4c21d4f217bd15d4799074
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-27 11:40:52 +0530

    Question added

---
+++ Solved Questions/Recursion/src/com/Burhan/Product_n_to_1.java
@@ -0,0 +1,15 @@
+package com.Burhan;
+
+public class Product_n_to_1 {
+    public static void main(String[] args) {
+        int n = 4;
+        int ans = productNum(n);
+        System.out.println(ans);
+    }
+    static int productNum(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * productNum(n-1);
+    }
+}
