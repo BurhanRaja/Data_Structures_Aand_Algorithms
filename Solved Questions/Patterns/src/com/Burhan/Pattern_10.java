575c13d43584c8028b632edc0c37bad88ad5878c
BurhanuddinRaja <mails4burhan@gmail.com>
2022-05-30 13:03:22 +0530

    one optimized and one added

--- Solved Questions/Patterns/src/com/Burhan/Pattern_10.java
+++ Solved Questions/Patterns/src/com/Burhan/Pattern_10.java
@@ -5,29 +5,36 @@ public class Pattern_10 {
         pattern(5);
     }
 
+    // Combining the patterns 11 and 12
     static void pattern(int n) {
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= n-i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = i+1; k > 1; k--) {
-                System.out.print("*");
-            }
-            for (int h = i-1; h >= 1; h--) {
-                System.out.print("*");
+        // Upper triangle i.e. rotated triangle by 180 degree
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = 1; j < 2*n; j++) {
+                if (i%2==0 && j>=n-i && j<=n+i && j%2!=0) {
+                    System.out.print("*");
+                }
+                else if (i%2!=0 && j>=n-i && j<=n+i && j%2==0) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
-
-        for (int l = 1; l <= n; l++) {
-            for (int m = 1; m <= n-l; m++) {
-                System.out.print(" ");
-            }
-            for (int o = 1; o < l+1; o++) {
-                System.out.print("*");
-            }
-            for (int p = 1; p <= l-1; p++) {
-                System.out.print("*");
+        
+        // Lower triangle i.e. simple triangle
+        for (int i = 0; i < 5; i++) {
+            for (int j = 1; j < 2*n; j++) {
+                if (i%2==0 && j>=n-i && j<=n+i && j%2!=0) {
+                    System.out.print("*");
+                }
+                else if (i%2!=0 && j>=n-i && j<=n+i && j%2==0) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
