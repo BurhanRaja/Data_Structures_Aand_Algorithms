4ae40514be2dc2573ab70379652eb2726f6a4fb8
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-02 17:35:05 +0530

    Question added

---
+++ Solved Questions/Recursion/src/com/Burhan/String/Remove_letter.java
@@ -0,0 +1,23 @@
+package com.Burhan.String;
+
+public class Remove_letter {
+    public static void main(String[] args) {
+        String answer = rmvL("baccad", "", 0);
+        String s = "abcd";
+        System.out.println(answer);
+    }
+
+    static String rmvL(String s, String ans, int n) {
+        if (n == s.length()) {
+            return ans;
+        }
+
+        char ch = s.trim().charAt(n);
+        if (ch != 'a') {
+            return rmvL(s, ans + ch, n+1);
+        }
+        else {
+            return rmvL(s, ans, n+1);
+        }
+    }
+}
