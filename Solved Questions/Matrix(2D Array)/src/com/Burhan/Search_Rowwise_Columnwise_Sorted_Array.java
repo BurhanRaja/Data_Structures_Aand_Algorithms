2c8d670a31a6164e2dfe7bb9f5d3c224c37c275d
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-02 16:49:51 +0530

    One Question added and one updated

--- Solved Questions/Matrix(2D Array)/src/com/Burhan/Search_Rowwise_Columnwise_Sorted_Array.java
+++ Solved Questions/Matrix(2D Array)/src/com/Burhan/Search_Rowwise_Columnwise_Sorted_Array.java
@@ -1,5 +1,6 @@
 package com.Burhan;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Search_Rowwise_Columnwise_Sorted_Array {
@@ -10,17 +11,24 @@ public class Search_Rowwise_Columnwise_Sorted_Array {
         //     {27, 29, 37, 48},
         //     {32, 33, 39, 50}
         // };
+        // int[][] arr = {
+        //     {1, 2, 3, 4},
+        //     {5, 6, 7, 8},
+        //     {9, 10, 11, 12},
+        //     {13, 14, 15, 16}
+        // };
         int[][] arr = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16}
+            {1,3,5,7},
+            {10,11,16,20}, 
+            {23,30,34,60}
         };
 
-        int x = 6;
+        int x = 13;
         int[] ans = search(arr, x);
         System.out.println(Arrays.toString(ans));
+        ArrayList<Integer> list = new ArrayList<Integer>();
 
+        // System.out.println(searching(arr, x));
         // Why top right or bottom left to start from?
         // Becoz the top left corner is the smallest element if x is greater than 10 we cannot decide where to go and the same reason goes for the bottom right corner it is greater than all the elements.
     }
@@ -30,6 +38,9 @@ public class Search_Rowwise_Columnwise_Sorted_Array {
         int countCol = arr[arr.length-1].length-1;
 
         while (countRow <= countCol) {
+            if (countRow > arr.length-1 || countCol < 0) {
+                return new int[] {-1, -1};
+            }
             if (x == arr[countRow][countCol]) {
                 return new int[] {countRow, countCol};
             }
@@ -42,4 +53,23 @@ public class Search_Rowwise_Columnwise_Sorted_Array {
         }
         return new int[] {-1, -1};
     }
+
+    // static boolean searching(int[][] matrix, int target) {
+    //     int row = 0;
+    //     int col = matrix[row].length-1;
+    //     while (row <= matrix.length-1 && col >= 0) {
+            
+    //         if (target == matrix[row][col]) {
+    //             return true;
+    //         }
+            
+    //         if (target > matrix[row][col]) {
+    //             row++;
+    //         }
+    //         else {
+    //             col--;
+    //         }
+    //     }
+    //     return false;
+    // }
 }
