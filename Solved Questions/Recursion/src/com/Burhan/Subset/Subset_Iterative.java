25fd3eebb59717a96b8efe1fb2bd75b1511d782b
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-04 17:21:03 +0530

    QUestion added and one modified

---
+++ Solved Questions/Recursion/src/com/Burhan/Subset/Subset_Iterative.java
@@ -0,0 +1,18 @@
+package com.Burhan.Subset;
+
+import java.util.ArrayList;
+
+public class Subset_Iterative {
+    public static void main(String[] args) {
+        String s = "abc";
+        System.out.println(s.substring(1, 3));
+    }
+
+    static void subseqIter(String s) {
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            
+        }
+    }
+}
