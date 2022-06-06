54553b006b82ae55325c4cbe6c0ec9acda453bb5
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-06 10:03:44 +0530

    QUestion added

--- Solved Questions/Recursion/src/com/Burhan/Subset/Subset_Iterative.java
+++ Solved Questions/Recursion/src/com/Burhan/Subset/Subset_Iterative.java
@@ -1,18 +1,31 @@
 package com.Burhan.Subset;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Subset_Iterative {
     public static void main(String[] args) {
-        String s = "abc";
-        System.out.println(s.substring(1, 3));
+        int[] arr = {1,2,3};
+
+        List<List<Integer>> ans = subseqIter(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.println(ans.get(i));
+        }
     }
 
-    static void subseqIter(String s) {
-        ArrayList<String> ans = new ArrayList<>();
+    static List<List<Integer>> subseqIter(int[] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
 
-        for (int i = 0; i < s.length(); i++) {
-            
+        for (int num : arr) {
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
         }
+
+        return outer;
     }
 }
