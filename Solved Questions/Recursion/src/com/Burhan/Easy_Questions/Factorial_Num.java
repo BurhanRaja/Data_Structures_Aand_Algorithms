04ec4ca17c38307ab59665db4a2e4bd9e85eaa6b
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-08 17:35:20 +0530

    Question added

---
+++ Solved Questions/Recursion/src/com/Burhan/Easy_Questions/Factorial_Num.java
@@ -0,0 +1,15 @@
+package com.Burhan.Easy_Questions;
+
+public class Factorial_Num {
+    public static void main(String[] args) {
+        int ans = factorial(5);
+        System.out.println(ans);
+    }
+
+    static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n*factorial(n-1);
+    }
+}
