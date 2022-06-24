c15d82da8ecbbaf0ffa62502c12ff3f1b7ea7137
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-24 16:59:15 +0530

    Question added

---
+++ Solved Questions/Hashing/src/com/Burhan/Subarray_Zero.java
@@ -0,0 +1,49 @@
+package com.Burhan;
+
+import java.util.HashSet;
+
+public class Subarray_Zero {
+    public static void main(String[] args) {
+        int[] arr = {1,4,13,-3,-10,5};
+        String ans = effSubArr(arr);
+        System.out.println(ans);
+    }
+
+    static String subArr(int[] arr) {
+        
+        if (arr.length == 1) {
+            if (arr[0] == 0) {
+                return "Yes";
+            }
+        }
+        // {1,4,13,-3,-10,5}
+        for (int i=0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum == 0) {
+                    return "Yes";
+                }
+            }
+        }
+        
+        return "No";
+    }
+
+    static String effSubArr(int[] arr) {
+        int sum = 0;
+        HashSet<Integer> h = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (h.contains(sum)) {
+                return "Yes";
+            }
+            else {
+                h.add(sum);
+            }
+        }
+
+        return "No";
+    }
+}
