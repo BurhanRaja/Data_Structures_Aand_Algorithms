c0eeb17d61195cf3027b1f725914b891c5371cae
BurhanuddinRaja <mails4burhan@gmail.com>
2022-07-17 10:44:58 +0530

    Question added

--- Solved Questions/Linked list/src/com/Burhan/Middle_of_Linkedlist.java
+++ Solved Questions/Linked list/src/com/Burhan/Middle_of_Linkedlist.java
@@ -4,17 +4,16 @@ public class Middle_of_Linkedlist {
     public static void main(String[] args) {
         Node head = new Node(10);
         head = insertAtEnd(head, 20);
-        head = insertAtEnd(head, 20);
         head = insertAtEnd(head, 30);
         head = insertAtEnd(head, 40);
-        head = insertAtEnd(head, 50);
 
         printList(head);
         System.out.println();
         MiddleNode(head);
-
+        middleNode(head);
     }
 
+    // Answer :- Naive
     static void MiddleNode(Node head) {
         if (head == null) {
             return;
@@ -32,6 +31,25 @@ public class Middle_of_Linkedlist {
         }
         System.out.println(curr.data);
     }
+
+    // Answer:- Efficient
+    static void middleNode(Node head) {
+        if (head == null) {
+            return;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println(slow.data);
+    }
+
+
 
     static Node insertAtEnd(Node head, int x) {
         Node temp = new Node(x);
