1aa373d9244548324da7f6ddc7b87cf98bd86af5
BurhanuddinRaja <mails4burhan@gmail.com>
2022-07-18 12:08:43 +0530

    Stack Implementation updated

--- Stack/src/com/Burhan/Stack_Implementation.java
+++ Stack/src/com/Burhan/Stack_Implementation.java
@@ -1,10 +1,14 @@
 package com.Burhan;
 
-import java.util.Stack;
+import java.util.*;
 
 public class Stack_Implementation {
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
+        // These both are same but Stack class inherits from Vector class, also called legacy class means present before collection class, is a thread safe class which can cause extra overhead in single thread env.
+
+        // Stack<Integer> s = new Stack<>();
+        
+        ArrayDeque<Integer> s = new ArrayDeque<>(); // used for single thread environment
         s.push(10);
         s.push(20);
         s.push(30);
