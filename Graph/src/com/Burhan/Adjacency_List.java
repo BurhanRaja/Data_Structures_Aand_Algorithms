574b777d8d3072b8df0927d120a6a64b5003f0e1
BurhanuddinRaja <mails4burhan@gmail.com>
2022-08-26 11:41:57 +0530

    Adjacency List added

---
+++ Graph/src/com/Burhan/Adjacency_List.java
@@ -0,0 +1,37 @@
+package com.Burhan;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Adjacency_List {
+    public static void main(String[] args) {
+        
+        int v= 5;
+        
+        ArrayList<LinkedList<Integer>> adj = new ArrayList<LinkedList<Integer>>(v);
+        
+        for (int i = 0; i < v; i++) {
+            adj.add(new LinkedList<Integer>());
+        }
+        
+        addEdge(adj, 0, 1);
+        addEdge(adj, 0, 2);
+        addEdge(adj, 1, 2);
+        addEdge(adj, 1, 3);
+
+        printGraph(adj);
+    }
+    static void addEdge(ArrayList<LinkedList<Integer>> adj, int u, int v) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    static void printGraph(ArrayList<LinkedList<Integer>> adj) {
+        for (int i = 0; i < adj.size(); i++) {
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(i + " - " + adj.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
