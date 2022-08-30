5fafd927625150759e3ad7a50599007f89aee1c2
BurhanuddinRaja <mails4burhan@gmail.com>
2022-08-30 19:32:25 +0530

    All Questions removed

--- Solved Questions/Arrays/src/com/Burhan/Easy/Integer_Array_Zero/Explaination.md
+++
@@ -1,5 +0,0 @@
-package com.Burhan.Easy.Integer_Array_Zero;
-
-public class Explaination {
-    
-}
--- Solved Questions/Bitwise Operator/src/com/Burhan/Pascal_Triangle/Pascal_Triangle.java
+++
@@ -1,9 +0,0 @@
-package com.Burhan.Pascal_Triangle;
-
-public class Pascal_Triangle {
-    public static void main(String[] args) {
-        int n = 5;
-
-        System.out.println(1<<(n-1));
-    }
-}
